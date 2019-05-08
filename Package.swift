7c04dbd08af9fe2bb421eabfbcbdbadf9383cc66
bootoption <48445527+bootoption@users.noreply.github.com>
2019-05-08 11:53:09 +0100

    Version

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
         name: "bootoption",
         dependencies: [
                 // Dependencies declare other packages that this package depends on.
-                .package(path: "../BootoptionSupport"),
-                .package(path: "../Gaps")
+                .package(url: "https://github.com/bootoption/BootoptionSupport.git", from: "2.0.0"),
+                .package(url: "https://github.com/bootoption/Gaps.git", from: "2.0.0")
         ],
         targets: [
                 // Targets are the basic building blocks of a package. A target can define a module or a test suite.
